0494a39eda651eb656598335364321e94b8d6e8e
Danny Celis <cdanny86@gmail.com>
2023-01-23 16:47:25 -0500

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '12345678',
+    user: '???',
+    host: '???',
+    database: '???',
+    password: '???',
     port: 5432
 });
 
